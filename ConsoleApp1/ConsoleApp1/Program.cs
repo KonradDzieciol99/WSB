60c36b7eddb38300213785651d2007a0ca0132dc
KonradDzieciol99 <konrad990212@gmail.com>
2019-10-03 12:07:29 +0200

    gfhh

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dlugosc");
+            string a = Console.ReadLine();
+
+
+
+            Console.WriteLine("szerokos");
+            int bb = int.Parse(Console.ReadLine());
+
+            int aa = int.Parse(a);
+
+
+            Console.WriteLine("pole prostokąta to  {0} ",aa*bb);
+
+            Console.ReadKey();
+
+        }
+    }
+}
