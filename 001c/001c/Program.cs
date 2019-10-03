5605cc7696836d62d45820177dbc8ff845d13ac8
KonradDzieciol99 <konrad990212@gmail.com>
2019-10-03 11:10:41 +0200

    wds

---
+++ 001c/001c/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
