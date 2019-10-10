c4975471dc1482af1e0ab5d5e9efe8e9a23e2011
KonradDzieciol99 <56111006+KonradDzieciol99@users.noreply.github.com>
2019-10-10 12:12:10 +0200

    Add files via upload

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            /*
 
             Console.WriteLine("1 pole kwadratu");
             Console.WriteLine("2 pole koła");
@@ -52,7 +53,7 @@ namespace ConsoleApp3
 
                     if (double.TryParse(k, out promien))
                     {
-                        Console.WriteLine("pole koła to" + Math.PI*promien*promien);
+                        Console.WriteLine("pole koła to" +Math.Round(Math.PI*promien*promien, 3));
                     }
                     else
                     {
@@ -68,6 +69,57 @@ namespace ConsoleApp3
                     Console.WriteLine("!!!");
                     break;
             }
+            
+            
+    
+            while (true)
+            {
+
+                Console.WriteLine()
+
+                int[,] oceny = new int[3, 3];
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int z = 0; z < 3; z++)
+                    {
+                        Console.Write(oceny[i, z] + " ");
+                    }
+                    Console.WriteLine("");
+                }
+
+            }  
+            
+
+            Console.WriteLine("asdfas");
+            for (int i = 20; i >= 5; i--)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+            */
+            Console.WriteLine("wysokość choinki");
+            
+            string x;
+            x = Console.ReadLine();
+            int podajgwiazdki = int.Parse(x);
+            
+            //int.TryParse(x,out podajgwiazdki);
+            for (int i=1; i <= podajgwiazdki ; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
 
             Console.ReadKey();
 
