8b01de30ed157026c5493855f2a18a4b3051a258
KonradDzieciol99 <56111006+KonradDzieciol99@users.noreply.github.com>
2019-10-10 11:06:06 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1 pole kwadratu");
+            Console.WriteLine("2 pole koła");
+
+
+            string x = Console.ReadLine();
+            int b = int.Parse(x);
+
+
+            switch (b)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("pole kwadratu");
+                    Console.WriteLine("podaj bok a");
+             
+                    string q = Console.ReadLine();
+                    int bok;
+
+                    if(int.TryParse(q, out bok))
+                    {
+                        Console.WriteLine("pole to"+bok * bok);
+                    }
+                    else
+                    {
+                        Console.WriteLine("złe dane!!!");
+                    }
+
+
+
+                    break;
+                case 2:
+                    Console.WriteLine("pole kola");
+
+                    Console.Clear();
+                    Console.WriteLine("podaj promien r");
+
+                    string k = Console.ReadLine();
+                    double promien;
+
+                    if (double.TryParse(k, out promien))
+                    {
+                        Console.WriteLine("pole koła to" + Math.PI*promien*promien);
+                    }
+                    else
+                    {
+                        Console.WriteLine("złe dane!!!");
+                    }
+
+
+
+                    break;
+                case 3:
+                    break;
+                default:
+                    Console.WriteLine("!!!");
+                    break;
+            }
+
+            Console.ReadKey();
+
+
+            
+
+
+
+        }
+    }
+}
